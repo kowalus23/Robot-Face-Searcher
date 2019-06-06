46902ff8e6b4e74e4373a2973e523f19ed1450fc
kowalus23 <kowalus23@gmail.com>
2019-06-06 18:17:33 +0200

    made CardList to keep smaller components in + loop to create robots

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = (props) => {
-    return (
-      <div className="tc bg-light-green dib br3 pa3 ma2 grow  bw2 shadow-5">
-          <img src={props.picture} alt="robot"/>
-          <div>
-              <h2>{props.name}</h2>
-              <p>{props.email}</p>
-          </div>
+const Card = ({name, email, id}) => {
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow  bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robot"/>
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
-    )
+    </div>
+  )
 };
 
 export default Card;
